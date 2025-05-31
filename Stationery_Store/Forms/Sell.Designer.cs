9756f538f0239eeebcb3ffe1cdba24c80059fcf6
Ahmed Khaled <ahmed.khaled.sayed2000@gmail.com>
2025-05-31 20:47:20 +0300

    Sell Form Concept demo

--- Stationery_Store/Forms/Sell.Designer.cs
+++ Stationery_Store/Forms/Sell.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class SellForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,27 +15,68 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            SellGridView = new DataGridView();
+            AddBtn = new Button();
+            productsGridView = new DataGridView();
             SearchTextBox = new TextBox();
             label1 = new Label();
-            searchBtn = new Button();
-            productsGridView = new DataGridView();
             panel1 = new Panel();
-            panel2 = new Panel();
-            button1 = new Button();
+            SellBtn = new Button();
+            TotalLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)SellGridView).BeginInit();
             ((System.ComponentModel.ISupportInitialize)productsGridView).BeginInit();
             panel1.SuspendLayout();
-            panel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // SellGridView
+            // 
+            SellGridView.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            SellGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            SellGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            SellGridView.Location = new Point(557, 11);
+            SellGridView.Margin = new Padding(3, 4, 3, 4);
+            SellGridView.Name = "SellGridView";
+            SellGridView.RightToLeft = RightToLeft.Yes;
+            SellGridView.RowHeadersWidth = 51;
+            SellGridView.Size = new Size(504, 385);
+            SellGridView.TabIndex = 7;
+            // 
+            // AddBtn
+            // 
+            AddBtn.BackColor = Color.DarkGray;
+            AddBtn.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            AddBtn.Location = new Point(122, 402);
+            AddBtn.Name = "AddBtn";
+            AddBtn.Size = new Size(165, 41);
+            AddBtn.TabIndex = 2;
+            AddBtn.Text = "إضافة";
+            AddBtn.TextImageRelation = TextImageRelation.ImageAboveText;
+            AddBtn.UseVisualStyleBackColor = false;
+            AddBtn.Click += AddBtn_Click;
+            // 
+            // productsGridView
+            // 
+            productsGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            productsGridView.Location = new Point(11, 132);
+            productsGridView.Name = "productsGridView";
+            productsGridView.RightToLeft = RightToLeft.Yes;
+            productsGridView.RowHeadersWidth = 51;
+            productsGridView.Size = new Size(540, 264);
+            productsGridView.TabIndex = 3;
             // 
             // SearchTextBox
             // 
-            SearchTextBox.Location = new Point(61, 86);
+            SearchTextBox.Location = new Point(78, 85);
             SearchTextBox.Name = "SearchTextBox";
             SearchTextBox.Size = new Size(280, 27);
             SearchTextBox.TabIndex = 0;
@@ -51,65 +85,48 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(61, 22);
+            label1.Font = new Font("Segoe UI", 19.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(87, 24);
             label1.Name = "label1";
-            label1.Size = new Size(276, 46);
+            label1.Size = new Size(251, 45);
             label1.TabIndex = 1;
-            label1.Text = "ابحث بأسم الصنف";
-            // 
-            // searchBtn
-            // 
-            searchBtn.BackColor = Color.DarkGray;
-            searchBtn.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            searchBtn.Location = new Point(119, 290);
-            searchBtn.Name = "searchBtn";
-            searchBtn.Size = new Size(165, 41);
-            searchBtn.TabIndex = 2;
-            searchBtn.Text = "بحث";
-            searchBtn.TextImageRelation = TextImageRelation.ImageAboveText;
-            searchBtn.UseVisualStyleBackColor = false;
-            searchBtn.Click += searchBtn_Click;
-            // 
-            // productsGridView
-            // 
-            productsGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            productsGridView.Location = new Point(3, 6);
-            productsGridView.Name = "productsGridView";
-            productsGridView.RowHeadersWidth = 51;
-            productsGridView.Size = new Size(394, 339);
-            productsGridView.TabIndex = 3;
+            label1.Text = "ابحث بأسم المنتج";
             // 
             // panel1
             // 
+            panel1.Controls.Add(TotalLabel);
+            panel1.Controls.Add(SellBtn);
+            panel1.Controls.Add(SellGridView);
             panel1.Controls.Add(label1);
             panel1.Controls.Add(SearchTextBox);
-            panel1.Controls.Add(searchBtn);
-            panel1.Location = new Point(1, 6);
+            panel1.Controls.Add(productsGridView);
+            panel1.Controls.Add(AddBtn);
+            panel1.Location = new Point(1, -5);
             panel1.Name = "panel1";
-            panel1.Size = new Size(400, 347);
+            panel1.Size = new Size(1064, 500);
             panel1.TabIndex = 4;
             // 
-            // panel2
+            // SellBtn
             // 
-            panel2.BackColor = SystemColors.GradientActiveCaption;
-            panel2.Controls.Add(productsGridView);
-            panel2.Location = new Point(406, 6);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(397, 348);
-            panel2.TabIndex = 5;
+            SellBtn.BackColor = Color.Green;
+            SellBtn.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            SellBtn.Location = new Point(649, 403);
+            SellBtn.Name = "SellBtn";
+            SellBtn.Size = new Size(295, 53);
+            SellBtn.TabIndex = 4;
+            SellBtn.Text = "بيع";
+            SellBtn.TextImageRelation = TextImageRelation.ImageAboveText;
+            SellBtn.UseVisualStyleBackColor = false;
+            SellBtn.Click += SellBtn_Click;
             // 
-            // button1
+            // TotalLabel
             // 
-            button1.BackColor = Color.Green;
-            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(623, 372);
-            button1.Name = "button1";
-            button1.Size = new Size(165, 41);
-            button1.TabIndex = 4;
-            button1.Text = "بيع";
-            button1.TextImageRelation = TextImageRelation.ImageAboveText;
-            button1.UseVisualStyleBackColor = false;
+            TotalLabel.AutoSize = true;
+            TotalLabel.Location = new Point(924, 356);
+            TotalLabel.Name = "TotalLabel";
+            TotalLabel.Size = new Size(49, 20);
+            TotalLabel.TabIndex = 6;
+            TotalLabel.Text = "1234$";
             // 
             // SellForm
             // 
@@ -117,28 +134,29 @@
             AutoScaleMode = AutoScaleMode.Font;
             AutoSize = true;
             BackColor = SystemColors.GradientInactiveCaption;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Controls.Add(panel2);
+            ClientSize = new Size(1070, 532);
             Controls.Add(panel1);
             IsMdiContainer = true;
             Name = "SellForm";
+            SizeGripStyle = SizeGripStyle.Show;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Sell";
+            ((System.ComponentModel.ISupportInitialize)SellGridView).EndInit();
             ((System.ComponentModel.ISupportInitialize)productsGridView).EndInit();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            panel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
+        private DataGridView SellGridView;
+        private Button AddBtn;
+        private DataGridView productsGridView;
         private TextBox SearchTextBox;
         private Label label1;
-        private Button searchBtn;
-        private DataGridView productsGridView;
         private Panel panel1;
-        private Panel panel2;
-        private Button button1;
+        private Button SellBtn;
+        private Label TotalLabel;
     }
 }
--- Stationery_Store/Forms/Sell.cs
+++ Stationery_Store/Forms/Sell.cs
@@ -1,38 +1,291 @@
 ﻿using Stationery_Store.Entities;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Stationery_Store.Forms
 {
     public partial class SellForm : Form
     {
+        private Context context = new Context();
+
+        // قائمة المنتجات التي أضافها المستخدم للبيع (السلة)
+        private List<Product> selectedProducts = new List<Product>();
+
         public SellForm()
         {
             InitializeComponent();
-        }
-        Context context = new Context();
-        private void searchBtn_Click(object sender, EventArgs e)
-        {
-           
+
+            // تعطيل توليد الأعمدة التلقائي لجداول البيانات
+            productsGridView.AutoGenerateColumns = false;
+
+            ConfigureProductGrid();
+            ConfigureSellGrid();
+            SellGridView.CellValueChanged += SellGridView_CellValueChanged;
+            SellGridView.EditingControlShowing += SellGridView_EditingControlShowing;
         }
 
+        /// <summary>
+        /// دالة البحث عن المنتجات عند كتابة نص في مربع البحث
+        /// </summary>
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             string searchTerm = SearchTextBox.Text.Trim();
 
-            // Search For Products
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                productsGridView.DataSource = null;
+                return;
+            }
+
+            // البحث في قاعدة البيانات عن المنتجات التي تحتوي أسمها على نص البحث
             var results = context.Products
                 .Where(p => p.Name.Contains(searchTerm))
+                .Take(50)
                 .ToList();
 
             productsGridView.DataSource = results;
         }
+
+        /// <summary>
+        /// دالة إضافة المنتج المختار من قائمة المنتجات إلى قائمة البيع
+        /// </summary>
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            if (productsGridView.CurrentRow == null)
+            {
+                MessageBox.Show("من فضلك اختر منتج لإضافته");
+                return;
+            }
+
+            var selectedProduct = (Product)productsGridView.CurrentRow.DataBoundItem;
+
+            if (selectedProducts.Any(p => p.ID == selectedProduct.ID))
+            {
+                MessageBox.Show("المنتج مضاف بالفعل في السلة");
+                return;
+            }
+
+            selectedProducts.Add(selectedProduct);
+
+            var price = selectedProduct.Price;
+
+            // إضافة الصف مع الـ ID مخفي
+            int rowIndex = SellGridView.Rows.Add();
+            var row = SellGridView.Rows[rowIndex];
+            row.Cells["SellProductId"].Value = selectedProduct.ID;
+            row.Cells["SellProductName"].Value = selectedProduct.Name;
+            row.Cells["SellQuantity"].Value = 1;
+            row.Cells["SellPrice"].Value = price;
+        }
+
+
+        /// <summary>
+        /// دالة تنفيذ عملية البيع عند الضغط على زر البيع
+        /// </summary>
+        private void SellBtn_Click(object sender, EventArgs e)
+        {
+            if (SellGridView.Rows.Count == 0)
+            {
+                MessageBox.Show("لا يوجد منتجات للبيع");
+                return;
+            }
+
+            bool hasError = false;
+
+            for (int i = 0; i < SellGridView.Rows.Count; i++)
+            {
+                var row = SellGridView.Rows[i];
+
+                string productName = row.Cells["SellProductName"].Value?.ToString();
+
+                //if (row.Cells["SellProductId"].Value == null || string.IsNullOrWhiteSpace(row.Cells["SellProductId"].Value.ToString()))
+                //{
+                //    MessageBox.Show("رقم المنتج غير موجود. تأكد من إدخال البيانات بشكل صحيح.");
+                //    hasError = true;
+                //    continue;
+                //}
+
+                int productId = int.Parse(row.Cells["SellProductId"].Value.ToString());
+                Console.WriteLine("productName ==> "+ productName);
+                int quantity;
+                decimal price;
+
+                // التأكد من صحة الكمية المدخلة
+                if (!int.TryParse(row.Cells["SellQuantity"].Value?.ToString(), out quantity) || quantity <= 0)
+                {
+                    MessageBox.Show($"برجاء إدخال كمية صحيحة للمنتج: {productName}");
+                    hasError = true;
+                    continue;
+                }
+
+                // التأكد من صحة السعر المدخل (يمكن أن يكون عشري)
+                if (!decimal.TryParse(row.Cells["SellPrice"].Value?.ToString(), out price) || price <= 0)
+                {
+                    MessageBox.Show($"برجاء إدخال سعر صحيح للمنتج: {productName}");
+                    hasError = true;
+                    continue;
+                }
+
+                // إيجاد المنتج في قاعدة البيانات باستخدام الاسم (الأفضل استخدام ID لو متوفر)
+                var product = context.Products.FirstOrDefault(p => p.ID == productId);
+
+                if (product == null)
+                {
+                    MessageBox.Show($"المنتج غير موجود في قاعدة البيانات: {productName}");
+                    hasError = true;
+                    continue;
+                }
+
+                // التحقق من توفر الكمية المطلوبة
+                if (quantity > product.Quantity)
+                {
+                    MessageBox.Show($"الكمية المطلوبة غير متاحة للمنتج: {productName}");
+                    hasError = true;
+                    continue;
+                }
+
+                // تحديث الكمية المتبقية في قاعدة البيانات
+                product.Quantity -= quantity;
+
+                // يمكن هنا إضافة سجل بيع أو فاتورة حسب نظامك
+                // كود إضافي لمعالجة البيع يمكن إضافته هنا
+            }
+
+            if (!hasError)
+            {
+                context.SaveChanges();
+                MessageBox.Show("تمت عملية البيع بنجاح");
+
+                // إعادة تهيئة الحقول بعد البيع
+                selectedProducts.Clear();
+                SellGridView.Rows.Clear();
+                SearchTextBox.Clear();
+                productsGridView.DataSource = null;
+            }
+            UpdateTotalPrice();
+        }
+
+        /// <summary>
+        /// تهيئة أعمدة جدول المنتجات (productsGridView)
+        /// </summary>
+        private void ConfigureProductGrid()
+        {
+            productsGridView.Columns.Clear();
+
+            productsGridView.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "الاسم",
+                DataPropertyName = "Name"
+            });
+
+            productsGridView.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "الوصف",
+                DataPropertyName = "Description"
+            });
+
+            productsGridView.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "السعر",
+                DataPropertyName = "Price"
+            });
+
+            productsGridView.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "الكمية المتاحة",
+                DataPropertyName = "Quantity"
+            });
+        }
+
+        /// <summary>
+        /// تهيئة أعمدة جدول البيع (SellGridView) مع السماح بتعديل العمودين "الكمية" و"السعر"
+        /// </summary>
+        private void ConfigureSellGrid()
+        {
+            SellGridView.Columns.Clear();
+
+            // عمود الـ ID (مخفي)
+            SellGridView.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "SellProductId",
+                Visible = false // نخفي العمود عشان ما يظهرش للمستخدم
+            });
+
+            SellGridView.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "اسم المنتج",
+                Name = "SellProductName",
+                ReadOnly = true
+            });
+
+            SellGridView.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "الكمية",
+                Name = "SellQuantity",
+                ReadOnly = false
+            });
+
+            SellGridView.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "السعر",
+                Name = "SellPrice",
+                ReadOnly = false
+            });
+        }
+
+
+        // يمكنك هنا إضافة أحداث مثل التحقق من المدخلات أثناء تحرير الخلايا لو حبيت
+
+
+
+        //Total Salary
+
+        private void UpdateTotalPrice()
+        {
+            decimal total = 0;
+
+            foreach (DataGridViewRow row in SellGridView.Rows)
+            {
+                if (row.IsNewRow) continue; // تجاهل صف الإدخال الجديد
+
+                if (decimal.TryParse(row.Cells["SellPrice"].Value?.ToString(), out decimal price) &&
+                    int.TryParse(row.Cells["SellQuantity"].Value?.ToString(), out int quantity))
+                {
+                    total += price * quantity;
+                }
+            }
+
+            TotalLabel.Text = $"الإجمالي: {total} جنيه";
+        }
+
+        private void SellGridView_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.TextChanged -= CellTextChanged; // مهم جدًا عشان ميكررش الحدث
+            e.Control.TextChanged += CellTextChanged;
+        }
+
+        private void CellTextChanged(object sender, EventArgs e)
+        {
+            UpdateTotalPrice();
+        }
+
+        private void SellGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 &&
+                (SellGridView.Columns[e.ColumnIndex].Name == "SellQuantity" ||
+                 SellGridView.Columns[e.ColumnIndex].Name == "SellPrice"))
+            {
+                UpdateTotalPrice();
+            }
+        }
+
+
+
+
+
     }
 }
