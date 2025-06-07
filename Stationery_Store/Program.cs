b29f74e97071c8eaf708fbd3b23a21f4498b02e4
Ahmed Khaled <ahmed.khaled.sayed2000@gmail.com>
2025-06-07 17:57:43 +0300

    solve error show in main

--- Stationery_Store/Program.cs
+++ Stationery_Store/Program.cs
@@ -13,7 +13,7 @@ namespace Stationery_Store
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
+//<<<<<<< HEAD
             Application.Run(new ProductsForm());
             //CategoryForm
             //SellForm
